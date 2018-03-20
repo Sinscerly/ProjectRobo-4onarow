def8c9cd5aee6da8a006d03421bbe17c13f9aae4
Sinscerly <email.thomas.jansema@gmail.com>
2018-03-20 13:40:39 +0100

    Changes to AI, commented brandon's hardcoded 2 depth inplementation. Changed a variabel to a private variabel

--- Connect 4/src/Connect/AI.java
+++ Connect 4/src/Connect/AI.java
@@ -1,40 +1,41 @@
 package Connect;
 
 public class AI {
+	private int[] moves = new int[7];
 	private int dificulty;
 	AI(int set_dificulty) {
 		dificulty = set_dificulty;
 	}
 	int doSet(Object[][] grid) {
 		//get position y position of all colums.
-		int[] moves = new int[7];
 		int do_move = 7;
 		moves = Board.free_slots();
-		do_move = isWin(grid, moves, Board.red); 	//if there is a winning move AI will make that move or when 2 moves from win
+		do_move = isWin(grid, Board.red); 	//if there is a winning move AI will make that move or when 2 moves from win
 		if(do_move < 7) { return do_move; }
-		do_move = isWin(grid, moves, Board.yellow);	//if yellow can have a winning move AI will try to block it
+		do_move = isWin(grid, Board.yellow);	//if yellow can have a winning move AI will try to block it
 		if(do_move < 7) { return do_move; }
 		return 7;
 	}
 	/* Depth 1 search for the winning move */
-	int isWin(Object[][] grid, int[] moves, Object color) {
+	int isWin(Object[][] grid, Object color) {
 		for(int x = 0; x < 7; x++) {
-			if(moves[x] != -1 && check_2_to_win(grid, x, moves[x], color)) {
+			if(moves[x] != -1 && check_winning_move(grid, x, color)) {
 				return x;
 			}
-			if(moves[x] != -1 && check_winning_move(grid, x, moves[x], color)) {
-				return x;
-			}
+//			if(moves[x] != -1 && check_2_to_win(grid, x, moves[x], color)) {
+//				return x;
+//			} 
 		}
 		return 7;
 	}
 	/* check placement of piece, if it will be the winning move. */
-	boolean check_winning_move(Object[][] grid, int x, int y, Object color) {
-		grid = Board.add(grid, x, y, color);
+	boolean check_winning_move(Object[][] grid, int x, Object color) {
+		grid = Board.add(grid, x, moves[x], color);
 		boolean win = Rules.checkWin(grid, false);
-		Board.remove(grid, x, y);
+		Board.remove(grid, x, moves[x]);
 		return win;
 	}
+	/* Brandon's hardcoded variant for 2depth algoritme */
 	boolean check_2_to_win(Object[][] grid, int x, int y, Object color) {
 		boolean win = false;
 		//
--- Connect 4/src/Connect/main.java
+++ Connect 4/src/Connect/main.java
@@ -3,7 +3,7 @@ package Connect;
 public class main {
 	
 	public static void main(String[] args) {
-		Game startGame = new Game(0, 1);
+		Game startGame = new Game(0, 1); //Start player, Difficulty
 		int x = 0;
 		while(x == 0) {
 			System.out.println("A new game will be started!");
