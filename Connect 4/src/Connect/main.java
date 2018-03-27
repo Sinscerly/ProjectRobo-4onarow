bfe630f569c635c8c90734d970fca9220a621469
brandon <b.disseldorp@outlook.com>
2018-03-27 21:14:04 +0200

    no real change only name change

--- Connect 4/src/Connect/main.java
+++ Connect 4/src/Connect/main.java
@@ -1,18 +1,19 @@
 package Connect;
 
-public class main {
-	
+public class Main {
+
 	public static void main(String[] args) {
 		int difficulty = 0;
-		System.out.println("At what difficulty do you want to play? From 1 to 7 is recommended and the max is 41 but don't do it!");
-		difficulty = Game.scanner.nextInt();
-		Game startGame = new Game(Game.AI, difficulty); //Start player, Difficulty
 		int x = 0;
-		while(x == 0) {
+		System.out.println(
+				"At what difficulty do you want to play? From 1 to 7 is recommended and the max is 41 but don't do it!");
+		difficulty = Game.scanner.nextInt();
+		Game startGame = new Game(Game.AI, difficulty);
+		while (x == 0) {
 			System.out.println("A new game will be started!");
 			System.out.println("");
 			startGame.start();
 		}
 	}
-	
+
 }
