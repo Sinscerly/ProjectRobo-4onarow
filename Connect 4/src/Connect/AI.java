03bd688f110b72034723a7007bea636961303c01
brandon <b.disseldorp@outlook.com>
2018-06-05 13:59:50 +0200

    some changes in AI and semi-finished Algorithm

--- Connect 4/src/Connect/AI.java
+++ Connect 4/src/Connect/AI.java
@@ -1,19 +1,29 @@
 package Connect;
 
+import java.util.Random;
+
 public class AI {
 
 	AI() {
 	}
 
 	int doSet(Box[][] grid, Box ai, Box en) {
+		Random rand = new Random();
 		Box[][] copiedgrid = copyGrid(grid);
+		int[] possibleMoves = new int[42];
+		int bestPri = 0;
+		int index = 0;
 		int cFree;
-		String Ai;
-		if (ai == Board.red)
+		String Ai, En;
+		if (ai == Board.red) {
 			Ai = "Red";
-		else
+			En = "Yellow";
+		}
+		else {
 			Ai = "Yellow";
-		for (int i = 0; i < 8; i++) {
+			En = "Red";
+		}
+		for (int i = 0; i < 7; i++) {
 			cFree = Board.checkColumnEmpty(i, copiedgrid);
 			if (cFree != -1) {
 				copiedgrid[i][cFree] = ai;
@@ -21,6 +31,37 @@ public class AI {
 					return i;
 				}
 				copiedgrid[i][cFree] = Board.empty;
+			}
+		}
+		for (int i = 0; i < 7; i++) {
+			cFree = Board.checkColumnEmpty(i, copiedgrid);
+			if (cFree != -1) {
+				copiedgrid[i][cFree] = en;
+				if(GoodMoves.checkWinningCondition(copiedgrid) == En) {
+					return i;
+				}
+				copiedgrid[i][cFree] = Board.empty;
+			}
+		}
+		int[][] priority = new int[7][6];
+		for(int y = 0; y < 6; y++) {
+			for(int x = 0; x < 7; x++) {
+				if(x < 5)
+					priority[x][y] = ((x + 1) + (-y + 7));
+				else
+					priority[x][y] = (((-x + 6) + 1) + (-y + 7));
+			}
+		}
+		for (int i = 0; i < 7; i++) {
+			cFree = Board.checkColumnEmpty(i, copiedgrid);
+			if (cFree != -1) {
+				if(priority[i][cFree] <= 7) {
+					possibleMoves[index] = i;
+					index++;
+				}
+				else {
+					
+				}
 			}
 		}
 		return 0;
--- Connect 4/src/tests/Algoritm.java
+++ Connect 4/src/tests/Algoritm.java
@@ -5,12 +5,27 @@ public class Algoritm {
 
 	public static void main(String[] args) {
 		int possible4 = 69;
-		for(int y = 1; y < 8; y++) {
-			for(int x = 1; x < 9; x++) {
-				if(x == 1) {
-					
-				}
+		int[][] priority = new int[7][6];
+		for(int y = 0; y < 6; y++) {
+			for(int x = 0; x < 7; x++) {
+				if(x < 4)
+					priority[x][y] = ((x*2 + 1) + (-y + 7));
+				else
+					priority[x][y] = (((-x + 6)*2 + 1) + (-y + 7));
 			}
+		}
+		printBoard(priority);
+	}
+	
+	public static void printBoard(int[][] board) {
+		System.out.println("");
+		System.out.println("| 1   2   3   4   5   6   7 |");
+		for (int y = 0; y < 6; y++) {
+			System.out.print("| ");
+			for (int x = 0; x < 7; x++) {
+				System.out.print(board[x][y] + " | ");
+			}
+			System.out.println();
 		}
 	}
 	
