ac8eb8665114cdd92aa486f0bed2cb097752f09e
Sinscerly_Desktop <Sinscerly@users.noreply.github.com>
2018-06-19 13:17:19 +0200

    d

--- VisionCode/makepicture.py
+++ VisionCode/makepicture.py
@@ -11,7 +11,7 @@ import time
 def main():
 	#get current time to make a unique timestamp
     x_time = time.strftime("%Y%m%d_%H%M%S")
-    pic_n = (x_time + ".jpg")
+	pic_n = (x_time + ".jpg")
     #Picture will be saved in directory: pic
 	if (os.path.isdir("pic") == False):
 		os.system("mkdir pic")
