866e13a39d42c7146b26a454666cfaf7eeb01f71
Sinscerly_Desktop <Sinscerly@users.noreply.github.com>
2018-06-19 13:12:54 +0200

    convert check
    added check if the convert -resize is done properly

--- VisionCode/makepicture.py
+++ VisionCode/makepicture.py
@@ -9,24 +9,28 @@ import os
 import time
 
 def main():
+	#get current time to make a unique timestamp
     x_time = time.strftime("%Y%m%d_%H%M%S")
     pic_n = (x_time + ".jpg")
-    
-    #Picture is saved in pic
-    pic_n_loc = ("pic/" + pic_n)
-
+    #Picture will be saved in directory: pic
     if (os.path.isdir("pic") == False):
         os.system("mkdir pic")
-    try:
+	pic_n_loc = ("pic/" + pic_n)
+    #make picture of the board
+	try:
         os.system("raspistill -o " + pic_n_loc)
     if (os.path.exists(pic_n_loc) == False):
         sys.exit("Picture wasn't token")
     print("Picture is token, named: " + pic_n)
-	
+	#Get size of image
+	size_pic = os.path.getsize(pic_n_loc)
 	#For the convert command it is needed to have imagemagick to be installed.
     os.system("convert -resize 20% " + pic_n_loc + " " + pic_n_loc)
-    print("Picture is resized to 20% of original")
-
+	if (size_pic == os.path.getsize(pic_n_loc) or size_pic < os.path.getsize(pic_n_loc)):
+		sys.exit("Picture couldn't be resized, check if ImageMagick is installed")
+	else
+		print("Picture is resized to 20% of original")
+	:
 
 if __name__ == '__main__':
 	main()
