a53856e7d7ca633b9b55fc2981e5fa943f7a9305
Sinscerly_Desktop <Sinscerly@users.noreply.github.com>
2018-06-25 23:11:00 +0200

    execute vision java

--- VisionCode/test_exe.java
+++ VisionCode/test_exe.java
@@ -7,51 +7,15 @@ class execute_doALL
 	
 	public static void main(String args[])
 	{	
+		//execute_vision();
 		try {
-			read();
+			read_output();
 		} catch (Exception e) {
 			System.out.println(e.toString());
 			e.printStackTrace();
 		}
-		//execute_vision();	
+		//return something..
 	}
-	public static int read() throws FileNotFoundException
-  	{
-  		// We need to provide file path as the parameter:
-  		// double backquote is to avoid compiler interpret words
-  		// like \test as \t (ie. as a escape sequence)
-  		File file = new File("grid/output.txt");
- 
-  		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-  		    	String line;
-			System.out.println("Hi..");
-  		    	//Check if grid is not faulty.
-  		    	if ((line = br.readLine()) == "5") {
-				int l = 0; j
-				System.out.println("Hi");
-  		    		for (int y = 6; y >= 0; y--) {
-					String numbers = br.readLine();
-  		    			System.out.println(numbers);
-					for(int x = 0; x < 6; x ++) {
-						board[y][x] = Integer.parseInt(numbers.substring(x));
-					}
-  		    		}
-  		    	} else if ((line = br.readLine()) == "Fault") {
-  		    		System.out.println("Grid is not okay.");
-  		    	} else { System.out.println("Not good"); }
-  		} catch (Exception e) {
-  				System.out.println(e.toString());
-  				e.printStackTrace();
-  		}
-  		return 1;
-  		//
-		
-		//String whole = "something";
-		//String first = whole.substring(0, 1);
-		//System.out.println(first);
-
-
-  	}	
 	private static void execute_vision()
 	{
 		//This part of code is made to find out how executing a python script works from a Java program!
@@ -79,4 +43,41 @@ class execute_doALL
 		//System.out.println("\nI waited to finish my writing");
 		System.out.println("Java project can go further now");
 	}
+	public static int read_output() throws FileNotFoundException
+  	{
+  		// We need to provide file path as the parameter:
+  		// double backquote is to avoid compiler interpret words
+  		// like \test as \t (ie. as a escape sequence)
+  		File file = new File("grid/output.txt");
+ 
+  		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+  		    	String line;
+			System.out.println("Hi..");
+  		    	//Check if grid is not faulty.
+  		    	if ((line = br.readLine()) == "5") {
+  		    		int l = 0; j
+  		    		System.out.println("Hi");
+	    			for (int y = 6; y >= 0; y--) {
+	    				String numbers = br.readLine();
+	    				System.out.println(numbers);
+	    				for(int x = 0; x < 6; x ++) {
+	    					board[y][x] = Integer.parseInt(numbers.substring(x));
+	    				}
+	    			}
+  		    	} else if ((line = br.readLine()) == "Fault") {
+  		    		System.out.println("Grid is not okay.");
+  		    	} else { System.out.println("Not good"); }
+  		} catch (Exception e) {
+  				System.out.println(e.toString());
+  				e.printStackTrace();
+  		}
+  		return 1;
+  		//
+		
+		//String whole = "something";
+		//String first = whole.substring(0, 1);
+		//System.out.println(first);
+
+
+  	}	
 }
