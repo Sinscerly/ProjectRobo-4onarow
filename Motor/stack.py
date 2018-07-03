48666291c2fde57cfb498066896cbd6cc57b0cea
Thomas@Pi <email.thomas.jansema@gmail.com>
2018-07-03 11:50:29 +0200

    stack

--- Motor/stack.py
+++ Motor/stack.py
@@ -1,7 +1,7 @@
-import RPI.GPIO as GPIO
-import time as T
+import RPi.GPIO as GPIO
+import time
 
-GPIO.setwarnings(TRUE)
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
 
@@ -10,17 +10,15 @@ stack = GPIO.PWM(7, 50)
 
 #startpositie
 stack.start(2.5)
+time.sleep(0.5)
 
 #180 degrees change
-time.sleep(0.05)
-stack.ChangeDutyCycle(7.5)
-
-#wait
-time.sleep(1)
+stack.ChangeDutyCycle(12.5)
+time.sleep(0.5)
 
 #back to 0 degrees
 stack.ChangeDutyCycle(2.5)
-time.sleep(0.05)
+time.sleep(0.5)
 
 #exit
 stack.stop()
