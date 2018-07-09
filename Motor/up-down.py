c4af4d6292aba2a8e6efb2c56818663d513a4dcc
Sinscerly <email.thomas.jansema@gmail.com>
2018-07-09 11:43:30 +0200

    array update

--- Motor/up-down.py
+++ Motor/up-down.py
@@ -3,7 +3,7 @@ import time
 import sys
 import os
 
-pos = [7, 3.7, 3.3]
+pos = [7, 3.7]
 
 def main():
     move(pos[1])
@@ -17,7 +17,7 @@ def move(pos):
         xpos = pos
         if(i == 2):
             if(pos != 7):
-                xpos = pos[2]
+                xpos = 3.3
         os.system("python half-motor.py " + str(i) + " " + str(xpos))
     return 1
 
