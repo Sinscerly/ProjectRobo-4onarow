560c03543c81ad485c5b52b13b35c60f32fc32d5
Thomas@Pi <email.thomas.jansema@gmail.com>
2018-07-10 15:55:25 +0200

    awesome stacker

--- Motor/stack.py
+++ Motor/stack.py
@@ -3,17 +3,17 @@ import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
 
 #frequentie
-stack = GPIO.PWM(7, 50)
+stack = GPIO.PWM(19, 50)
 
 #startpositie
 stack.start(2.5)
 time.sleep(0.5)
 
 #180 degrees change
-stack.ChangeDutyCycle(12.5)
+stack.ChangeDutyCycle(7.8)
 time.sleep(0.5)
 
 #back to 0 degrees
